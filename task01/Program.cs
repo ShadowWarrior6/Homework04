c107262df39a7c58d2fb23719eb804618c6062ca
Andranik <andran1401@gmail.com>
2023-03-16 20:45:07 +0400

    task01

---
+++ task01/Program.cs
@@ -0,0 +1,13 @@
+﻿int Alaba(int num )
+{
+    int all_Alaba = 0;
+    for   (int i = 1; i<=num; i++ )
+        all_Alaba +=i;
+
+
+        return all_Alaba;
+}
+
+int A =int.Parse(Console.ReadLine()!);
+int result = Alaba(A);
+Console.WriteLine(result);
