c2026d0c3b9c02be47f7b06533cc1cd6b095935a
Andranik <andran1401@gmail.com>
2023-03-16 21:21:59 +0400

    task03

---
+++ task03/Program.cs
@@ -0,0 +1,13 @@
+﻿int productOfNumbers(int num)
+{
+    int all_sum = 1;
+    for (int i = 1; i <= num; i++)
+        all_sum *= i;
+
+    return all_sum;
+
+
+}
+int A = int.Parse(Console.ReadLine()!);
+int result = productOfNumbers(A);
+Console.WriteLine(result);
