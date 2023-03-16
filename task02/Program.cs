db447ef09f3c9be6798dbd8af185373040b5fa8e
Andranik <andran1401@gmail.com>
2023-03-16 21:04:14 +0400

    task02

---
+++ task02/Program.cs
@@ -0,0 +1,12 @@
+﻿int Srt(int num){
+    int kol = 0;
+
+    for (int i = 1;  num > 0; i++){
+        num = num/ 10;
+        kol=i;
+    }
+    return kol;
+}
+
+int A =int.Parse(Console.ReadLine()!);
+Console.WriteLine(Srt(A));
